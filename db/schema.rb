de29823954bcdc56c54288c85d0cdc30890a9a60
Al Snow <jasnow@hotmail.com>
2016-07-20 16:46:24 -0400

    Upgraded to Rails 5.0

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,31 +8,31 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222210613) do
+ActiveRecord::Schema.define(version: 20120222210613) do
 
-  create_table "favorites", :force => true do |t|
+  create_table "favorites", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.string   "website"
     t.text     "description"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.boolean  "approved",    :default => false, :null => false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "approved",    default: false, null: false
   end
 
-  create_table "guests", :force => true do |t|
+  create_table "guests", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "twitter"
     t.string   "phone"
     t.text     "bio"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "hosts", :force => true do |t|
+  create_table "hosts", force: :cascade do |t|
     t.string   "name"
     t.string   "twitter"
     t.string   "address1"
@@ -46,9 +45,9 @@ ActiveRecord::Schema.define(:version => 20120222210613) do
     t.integer  "number_of_guests_remaining"
     t.text     "description"
     t.string   "email"
-    t.datetime "created_at",                                    :null => false
-    t.datetime "updated_at",                                    :null => false
-    t.boolean  "approved",                   :default => false, :null => false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.boolean  "approved",                   default: false, null: false
   end
 
 end
