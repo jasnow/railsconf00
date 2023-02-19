6193879a633442ea2a0313df8d1237e63bfa5c7f
Al Snow <jasnow@hotmail.com>
2023-02-19 10:30:59 -0500

    Fixed to get clean 'rails server' run

--- db/schema.rb
+++ db/schema.rb
@@ -2,52 +2,51 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120222210613) do
-
+ActiveRecord::Schema[7.0].define(version: 2012_02_22_210613) do
   create_table "favorites", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "website"
-    t.text     "description"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "approved",    default: false, null: false
+    t.string "name"
+    t.string "address"
+    t.string "website"
+    t.text "description"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.boolean "approved", default: false, null: false
   end
 
   create_table "guests", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "twitter"
-    t.string   "phone"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
+    t.string "twitter"
+    t.string "phone"
+    t.text "bio"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "hosts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "twitter"
-    t.string   "address1"
-    t.string   "address2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.string   "phone"
-    t.integer  "number_of_guests"
-    t.integer  "number_of_guests_remaining"
-    t.text     "description"
-    t.string   "email"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-    t.boolean  "approved",                   default: false, null: false
+    t.string "name"
+    t.string "twitter"
+    t.string "address1"
+    t.string "address2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "phone"
+    t.integer "number_of_guests"
+    t.integer "number_of_guests_remaining"
+    t.text "description"
+    t.string "email"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.boolean "approved", default: false, null: false
   end
 
 end
